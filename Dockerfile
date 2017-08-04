5dc9df0a5b2f8594db351a5f8532328d95175445
janikibichi <janikibichi@gmail.com>
2017-08-04 15:53:53 +0300

    new lib

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+#start with a base image
+FROM ubuntu:16.04
+RUN apt-get update && apt-get -y upgrade
+#install python
+RUN apt-get -y install python && \
+    apt-get -y install python-pip && \
+    apt-get -y install nano 
+#install framework
+WORKDIR ussd/
+COPY . ussd/
+RUN cd ussd/python && pip install requirements.txt
+CMD ["python","sms.py"]
---
+++ sms.py
@@ -0,0 +1,28 @@
+from flask import Flask
+# Import the helper gateway class
+from AfricasTalkingGateway import AfricasTalkingGateway, AfricasTalkingGatewayException 
+
+app = Flask(__name__)
+
+@app.route("/sms")
+def sms():
+        # Specify your login credentials
+    username = "jkuat"
+    apikey   = "30e34507d42910e827b2ed32c6ffe53019f42feacf1a7e905c753196dd4454bd"
+
+    to      = "+254787235065,+254708415904"
+    message = "I'm a lumberjack and it's ok, I sleep all night and I work all day"
+    gateway = AfricasTalkingGateway(username, apikey)
+
+    try:
+    
+        results = gateway.sendMessage(to, message)
+        
+        for recipient in results:
+            # status is either "Success" or "error message"
+            print 'number=%s;status=%s;messageId=%s;cost=%s' % (recipient['number'],
+                                                                recipient['status'],
+                                                                recipient['messageId'],
+                                                                recipient['cost'])
+    except AfricasTalkingGatewayException, e:
+        print 'Encountered an error while sending: %s' % str(e)
